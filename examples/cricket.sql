20737d96b8d67cde08c72bded9d369b4bc9461c8
vinodh5052@gmail.com <vinodhkumar5052@gmail.com>
2025-08-17 15:32:40 +0000

    cc

---
+++ examples/cricket.sql
@@ -0,0 +1,317 @@
+use role sysadmin;
+use warehouse compute_wh;
+
+create database if not exists cricket;
+create schema if not exists cricket.land WITH MANAGED ACCESS;
+create schema if not exists cricket.raw WITH MANAGED ACCESS;
+create schema if not exists cricket.clean WITH MANAGED ACCESS;
+create schema if not exists cricket.consumption WITH MANAGED ACCESS;
+
+create or replace file format cricket.raw.cricket_json_format
+type=json
+compression=auto
+trim_space=true
+allow_duplicate=false
+null_if=('\\n', 'null', '')
+strip_outer_array=true
+strip_null_values=true
+comment='Custom JSON Format';
+
+create or replace stage cricket.raw.cricket_json_files;
+
+remove @cricket_json_files/cricket/json/2023-world-cup-json.zip;
+list @cricket.raw.cricket_json_files;
+
+
+create or replace transient table cricket.raw.match_raw_tbl (
+    meta object not null,
+    info variant not null,
+    innings ARRAY not null,
+    stg_file_name text not null,
+    stg_file_row_number int not null,
+    stg_file_hashkey text not null,
+    stg_modified_ts timestamp not null
+)
+comment = 'This is raw table to store all the json data file with root elements extracted'
+;
+
+copy into cricket.raw.match_raw_tbl from (
+select 
+        t.$1:meta::object as meta, 
+        t.$1:info::object as info, 
+        t.$1:innings::array as innings, 
+        metadata$filename as file_name,
+        metadata$file_row_number int,
+        metadata$file_content_key text,
+        metadata$file_last_modified stg_modified_ts
+from @cricket_json_files/cricket/json (FILE_FORMAT => 'cricket_json_format') T
+) on_error=continue;
+
+-- lets execute the count
+select count(*) from cricket.raw.match_raw_tbl;
+
+
+create or replace transient table cricket.clean.match_detail_clean as
+select
+    info:match_type_number::int as match_type_number, 
+    info:event.name::text as event_name,
+    case
+    when 
+        info:event.match_number::text is not null then info:event.match_number::text
+    when 
+        info:event.stage::text is not null then info:event.stage::text
+    else
+        'NA'
+    end as match_stage,   
+    info:dates[0]::date as event_date,
+    date_part('year', event_date) as event_year,
+    date_part('month', event_date) as event_month,
+    date_part('day', event_date) as event_day,
+    info:match_type::text as match_type,
+    info:season::text as season,
+    info:team_type::text as team_type,
+    info:overs::text as overs,
+    info:city::text as city,
+    info:venue::text as venue, 
+    info:gender::text as gender,
+    info:teams[0]::text as first_team,
+    info:teams[1]::text as second_team,
+    case 
+        when info:outcome.winner is not null then 'Result Declared'
+        when info:outcome.result = 'tie' then 'Tie'
+        when info:outcome.result = 'no result' then 'No Result'
+        else info:outcome.result
+    end as matach_result,
+    case 
+        when info:outcome.winner is not null then info:outcome.winner
+        else 'NA'
+    end as winner,   
+
+    info:toss.winner::text as toss_winner,
+    initcap(info:toss.decision::text) as toss_decision,
+    --
+    stg_file_name ,
+    stg_file_row_number,
+    stg_file_hashkey,
+    stg_modified_ts
+    from 
+    cricket.raw.match_raw_tbl;
+
+
+select * from cricket.clean.match_detail_clean limit 10;
+
+create or replace transient table cricket.clean.player_clean_tbl as 
+select 
+    rcm.info:match_type_number::int as match_type_number, 
+    p.path::text as country,
+    team.value:: text as player_name,
+    stg_file_name ,
+    stg_file_row_number,
+    stg_file_hashkey,
+    stg_modified_ts
+from cricket.raw.match_raw_tbl rcm,
+lateral flatten (input => rcm.info:players) p,
+lateral flatten (input => p.value) team;
+
+desc table cricket.clean.player_clean_tbl;
+
+alter table cricket.clean.player_clean_tbl
+modify column 
+    match_type_number set not null,
+    player_name set not null,
+    country set not null;
+
+alter table cricket.clean.player_clean_tbl
+add constraint fk_match_number
+foreign key(match_type_number) references cricket.clean.match_detail_clean (match_type_number);
+
+desc table cricket.clean.match_detail_clean;
+
+alter table cricket.clean.match_detail_clean
+add constraint pk_match_number primary key(match_type_number);
+
+select * from cricket.clean.player_clean_tbl;
+
+
+create or replace transient table cricket.clean.delivery_clean_tbl as
+select 
+    m.info:match_type_number::int as match_type_number, 
+    i.value:team::text as country,
+    o.value:over::int as over,
+    d.value:bowler::text as bowler,
+    d.value:batter::text as batter,
+    d.value:non_striker::text as non_striker,
+    d.value:runs.batter::text as runs,
+    d.value:runs.extras::text as extras,
+    d.value:runs.total::text as total,
+    w.value:player_out::text as player_out,
+    -- e.key::text as extra_type,
+    -- e.value::number as extra_runs,
+    w.value:kind::text as player_out_kind,
+    w.value:fielders[0].name::text as player_out_fielders,
+    m.stg_file_name ,
+    m.stg_file_row_number,
+    m.stg_file_hashkey,
+    m.stg_modified_ts
+from cricket.raw.match_raw_tbl m,
+lateral flatten (input => m.innings) i,
+lateral flatten (input => i.value:overs) o, 
+lateral flatten (input => o.value:deliveries) d,
+lateral flatten (input => d.value:extras, outer => True) e,
+lateral flatten (input => d.value:wickets, outer => True) w;
+
+
+select * from cricket.clean.delivery_clean_tbl  limit 10;
+
+
+alter table cricket.clean.delivery_clean_tbl
+modify column 
+    match_type_number set not null,
+    country set not null,
+    over set not null,
+    bowler set not null,
+    batter set not null,
+    non_striker set not null;
+
+-- fk relationship
+alter table cricket.clean.delivery_clean_tbl
+add constraint fk_delivery_match_id
+foreign key (match_type_number) references cricket.clean.match_detail_clean (match_type_number);
+
+
+select get_ddl('table', 'cricket.clean.match_detail_clean');
+
+select get_ddl('table', 'cricket.clean.player_clean_tbl');
+
+select get_ddl('table', 'cricket.clean.delivery_clean_tbl');
+
+
+
+
+create or replace transient table date_dim (
+    date_id int primary key autoincrement,
+    full_dt date,
+    day int,
+    month int,
+    year int,
+    quarter int,
+    dayofweek int,
+    dayofmonth int,
+    dayofyear int,
+    dayofweekname varchar(3), -- to store day names (e.g., "Mon")
+    isweekend boolean -- to indicate if it's a weekend (True/False Sat/Sun both falls under weekend)
+);
+
+create or replace transient table referee_dim (
+    referee_id int primary key autoincrement,
+    referee_name text not null,
+    referee_type text not null
+);
+
+create or replace transient table team_dim (
+    team_id int primary key autoincrement,
+    team_name text not null
+);
+
+
+create or replace transient table player_dim (
+    player_id int primary key autoincrement,
+    team_id int not null,
+    player_name text not null
+);
+
+alter table cricket.consumption.player_dim
+add constraint fk_team_player_id
+foreign key (team_id) references cricket.consumption.team_dim (team_id);
+
+create or replace transient table venue_dim (
+    venue_id int primary key autoincrement,
+    venue_name text not null,
+    city text not null,
+    state text,
+    country text,
+    continent text,
+    end_Names text,
+    capacity number,
+    pitch text,
+    flood_light boolean,
+    established_dt date,
+    playing_area text,
+    other_sports text,
+    curator text,
+    lattitude number(10,6),
+    longitude number(10,6)
+);
+
+create or replace transient table match_type_dim (
+    match_type_id int primary key autoincrement,
+    match_type text not null
+);
+
+
+CREATE or replace transient TABLE match_fact (
+    match_id INT PRIMARY KEY,
+    date_id INT NOT NULL,
+    referee_id INT NOT NULL,
+    team_a_id INT NOT NULL,
+    team_b_id INT NOT NULL,
+    match_type_id INT NOT NULL,
+    venue_id INT NOT NULL,
+    
+    total_overs number(3),
+    balls_per_over number(1),
+
+    overs_played_by_team_a number(2),
+    bowls_played_by_team_a number(3),
+    extra_bowls_played_by_team_a number(3),
+    extra_runs_scored_by_team_a number(3),
+    fours_by_team_a number(3),
+    sixes_by_team_a number(3),
+    total_score_by_team_a number(3),
+    wicket_lost_by_team_a number(2),
+
+    overs_played_by_team_b number(2),
+    bowls_played_by_team_b number(3),
+    extra_bowls_played_by_team_b number(3),
+    extra_runs_scored_by_team_b number(3),
+    fours_by_team_b number(3),
+    sixes_by_team_b number(3),
+    total_score_by_team_b number(3),
+    wicket_lost_by_team_b number(2),
+
+    toss_winner_team_id int not null, 
+    toss_decision text not null, 
+    match_result text not null, 
+    winner_team_id int not null,
+
+    CONSTRAINT fk_date FOREIGN KEY (date_id) REFERENCES date_dim (date_id),
+    CONSTRAINT fk_referee FOREIGN KEY (referee_id) REFERENCES referee_dim (referee_id),
+    CONSTRAINT fk_team1 FOREIGN KEY (team_a_id) REFERENCES team_dim (team_id),
+    CONSTRAINT fk_team2 FOREIGN KEY (team_b_id) REFERENCES team_dim (team_id),
+    CONSTRAINT fk_match_type FOREIGN KEY (match_type_id) REFERENCES match_type_dim (match_type_id),
+    CONSTRAINT fk_venue FOREIGN KEY (venue_id) REFERENCES venue_dim (venue_id),
+
+    CONSTRAINT fk_toss_winner_team FOREIGN KEY (toss_winner_team_id) REFERENCES team_dim (team_id),
+    CONSTRAINT fk_winner_team FOREIGN KEY (winner_team_id) REFERENCES team_dim (team_id)
+);
+
+CREATE or replace transient TABLE delivery_fact (
+    match_id INT ,
+    team_id INT,
+    bowler_id INT,
+    batter_id INT,
+    non_striker_id INT,
+    over INT,
+    runs INT,
+    extra_runs INT,
+    extra_type VARCHAR(255),
+    player_out VARCHAR(255),
+    player_out_kind VARCHAR(255),
+
+    CONSTRAINT fk_del_match_id FOREIGN KEY (match_id) REFERENCES match_fact (match_id),
+    CONSTRAINT fk_del_team FOREIGN KEY (team_id) REFERENCES team_dim (team_id),
+    CONSTRAINT fk_bowler FOREIGN KEY (bowler_id) REFERENCES player_dim (player_id),
+    CONSTRAINT fk_batter FOREIGN KEY (batter_id) REFERENCES player_dim (player_id),
+    CONSTRAINT fk_stricker FOREIGN KEY (non_striker_id) REFERENCES player_dim (player_id)
+);
+
